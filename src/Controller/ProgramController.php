413d6847654ab1dc81d1d0503e9086cde4ea4e23
Benjamin Zilliox <benjaminzilliox@gmail.com>
2023-05-24 12:12:25 +0200

    refonte graphique

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Repository\SeasonRepository;
 use App\Repository\EpisodeRepository;
 use App\Repository\ProgramRepository;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
